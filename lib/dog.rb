28f8380fb6cf99e45a8f3ec1b53b29b648c50e1c
Clarissa Jaime <clarissanjaime@gmail.com>
2018-04-23 01:19:16 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,6 @@
 # Your code goes here!
 class Dog
-  def initialize(name)
-    @name = name
-  end
-
-  def name=(name)
-    @name = name
-  end
-
-  def name
-    @name
-  end
+  attr_accessor(:name)
 
   def bark
     puts "woof!"
